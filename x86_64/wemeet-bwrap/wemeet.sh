dce7b31b951eef2b7037f029689e5eb849b6a8c6
sf467 <121373973+sf467@users.noreply.github.com>
2026-03-16 16:48:36 +0800

    update wemeet (#17)
    * update wemeet

--- x86_64/wemeet-bwrap/wemeet.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -eu
-set -m
-export QT_AUTO_SCREEN_SCALE_FACTOR=1
-export QT_STYLE_OVERRIDE=fusion # 解决使用自带 Qt 情况下，字体颜色全白看不到的问题
-export IBUS_USE_PORTAL=1        # fix ibus
-export LD_LIBRARY_PATH="/usr/lib/wemeet/lib:$PATH" # 否则启动失败
-export LD_PRELOAD="/usr/lib/wemeet/libhook.so"
-env -u XDG_SESSION_TYPE -u WAYLAND_DISPLAY QT_QPA_PLATFORM=xcb /opt/wemeet/bin/wemeetapp "$@"
